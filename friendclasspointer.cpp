be33066ebafadb58e529ceb7f60c43a77dfe16e5
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-04-28 16:49:40 +0700

    friendclasspointer

--- friendclasspointer.cpp
+++ friendclasspointer.cpp
@@ -9,3 +9,18 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{1}; // object mhs
+    mhs.showNim();    // member Access operstor
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+}
